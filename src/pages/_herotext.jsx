16a370de27022ca8a2a3506e02ccf4f09ac8eea7
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-25 15:06:16 +0400

    Update _herotext.jsx

--- src/pages/_herotext.jsx
+++ src/pages/_herotext.jsx
@@ -21,7 +21,7 @@ export const HeroText = ({ children, size }) => {
         fontSize: size,
       }}
     >
-      {children.split('').map((letter, key) => (
+      {data?.description.split('').map((letter, key) => (
         <motion.div key={key}>{letter}</motion.div>
       ))}
     </motion.div>
