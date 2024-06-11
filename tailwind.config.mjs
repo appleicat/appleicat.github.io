b6b2ce0f295552973eb967ac7d9d5af4d4c8f188
appleicat <appleicat@outlook.com>
2024-06-11 18:26:40 +0400

    set up tailwindcss typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [typography],
 };
