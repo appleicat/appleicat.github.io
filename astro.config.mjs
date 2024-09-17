4729629d551a6ee3bd8fefbdfec694f345eb79fa
appleicat <appleicat@outlook.com>
2024-09-17 22:09:15 +0400

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), vue()],
   site: 'https://appleicat.github.io',
   markdown: {
     remarkRehype: {
